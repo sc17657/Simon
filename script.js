86305e5467dbb5afb3e451949962256300a45257
sue2021 <carpentersuzanne@johndeere.com>
2021-08-02 18:27:15 -0500

    computer1

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ const Yellow = document.querySelector('.buttonYellow');
 const Blue = document.querySelector('.buttonBlue');
 const Green = document.querySelector('.buttonGreen');
 const Red = document.querySelector('.buttonRed');
+const Start = document.querySelector('.button2')
+// console.log(Start)
 
+
+function computer() {
+    Yellow.style.backgroundColor = "white"
+        setTimeout(turnWhite, 1600)
+    Green.style.backgroundColor = "white"
+        setTimeout(clickGreen, 1400)
+    Red.style.backgroundColor = "white"
+        setTimeout(clickRed, 1200)
+    Blue.style.backgroundColor = "white"
+        setTimeout(clickBlue, 1000)
+}
+Start.addEventListener("click", computer);
 
 
 
@@ -15,7 +29,7 @@ function turnWhite() {
     Yellow.style.backgroundColor = "yellow";
 }
 Yellow.addEventListener("click", playerMove);
-console.log(Yellow)
+// console.log(Yellow)
 
 
 
@@ -26,10 +40,10 @@ function playerMove1() {
    }
    function clickGreen() {
         Green.style.backgroundColor = "green";
-       console.log("Now I am red again")
+    //    console.log("Now I am red again")
    }
    Green.addEventListener("click", playerMove1);
-   console.log(Green)
+//    console.log(Green)
 
    function playerMove2() {
     Red.style.backgroundColor = "white";
@@ -37,10 +51,10 @@ function playerMove1() {
    }
    function clickRed() {
         Red.style.backgroundColor = "red";
-       console.log("Now I am red again")
+    //    console.log("Now I am red again")
    }
    Red.addEventListener("click", playerMove2);
-   console.log("red")
+//    console.log("red")
 
    function playerMove3() {
     Blue.style.backgroundColor = "white";
@@ -48,9 +62,10 @@ function playerMove1() {
    }
    function clickBlue() {
         Blue.style.backgroundColor = "blue";
-       console.log("Now I am blue again")
+    //    console.log("Now I am blue again")
    }
    Blue.addEventListener("click", playerMove3);
-   console.log("blue")
+//    console.log("blue")
 
-   
+        
+ 
